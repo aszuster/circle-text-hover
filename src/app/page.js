92110852ebbe78f85bd051bf318f3fbae7b04058
Alejandra <ale.szuster@gmail.com>
2024-08-03 23:44:59 -0300

    lag fix

--- src/app/page.js
+++ src/app/page.js
@@ -18,14 +18,14 @@ export default function Home() {
       }}
       transition={{ type: "tween", ease: "backOut", duration:0.5}}
       >
-        <p onMouseEnter={() =>{setIsHovered(true)}} onMouseLeave={() =>{setIsHovered(false)}} className="text-lilac-00 w-[1300px] p-[40px] font-noto font-medium text-[70px] leading-[80px]">
-          The bitter scent of lemons remains embedded in my heart <br />
-          I can&apos;t make my way home &apos;til the rain lets up <br />
+        <p onMouseEnter={() =>{setIsHovered(true)}} onMouseLeave={() =>{setIsHovered(false)}} className="text-lilac-00 w-[1300px] p-[40px] font-noto font-medium text-[60px] leading-[65px]">
+          The bitter scent of lemons remains embedded in my heart. 
+          I can&apos;t make my way home &apos;til the rain lets up. Even now you remain my light.
         </p>
       </motion.div>
 
       <div className="w-full h-full flex items-center justify-center">
-        <p className="text-lilac-00 w-[1300px] font-poppins font-medium text-[70px] leading-[80px]">
+        <p className="text-lilac-00 w-[1300px] font-poppins font-medium text-[70px] leading-[90px]">
           胸に残り離れない 苦いレモンの匂い <br />
           雨が降り止むまでは帰れない <br />
           今でもあなたはわたしの光
